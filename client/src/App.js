77e02992e9b2dc420df8593c21be1b5676c03903
Vijay2351989 <vjbhatt760@gmail.com>
2019-09-24 18:03:57 +0530

    checking kubernetes update

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
   <Router>
     <div className="App">
+        <h1>Fib Calculator</h1>
         <Link to="/">Home</Link>
         <Link to="/otherpage">Other Page</Link>
       <div>
